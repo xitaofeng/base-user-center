75b9a86648929ecd47c7cf10e566cacb29173133
罗军 <luojun@shsnc.com>
2017-06-06 14:45:01 +0800

    权限-权限信息管理-初始化版本

--- src/main/java/com/shsnc/authorization/service/AuthorizationInfoService.java
+++ src/main/java/com/shsnc/authorization/service/AuthorizationInfoService.java
@@ -5,6 +5,7 @@ import com.shsnc.authorization.config.AuthorizationConstant;
 import com.shsnc.authorization.mapper.AuthorizationInfoModelMapper;
 import com.shsnc.authorization.model.AuthorizationInfoModel;
 import com.shsnc.base.util.DateTimeUtil;
+import com.shsnc.base.util.config.BizException;
 import com.sun.org.apache.regexp.internal.REUtil;
 import org.omg.CORBA.SystemException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,11 +32,11 @@ public class AuthorizationInfoService {
      * @throws Exception
      */
     public AuthorizationInfoModel addAuthorizationInfo(AuthorizationInfoModel authorizationInfoModel) throws Exception {
-        if(isAuthorizationName(AuthorizationConstant.BasicOperation.ADD,null,authorizationInfoModel.getAuthorizationName())){
-            throw new Exception("权限名称重复");
+        if (isAuthorizationName(AuthorizationConstant.BasicOperation.ADD, authorizationInfoModel.getAuthorizationName())) {
+            throw new BizException("权限名称重复");
         }
-        if(isAuthorizationName(AuthorizationConstant.BasicOperation.ADD,null,authorizationInfoModel.getAuthorizationCode())){
-            throw new Exception("权限编码重复");
+        if (isAuthorizationName(AuthorizationConstant.BasicOperation.ADD, authorizationInfoModel.getAuthorizationCode())) {
+            throw new BizException("权限编码重复");
         }
         authorizationInfoModel.setCreateTime(new Date().getTime());
         if (authorizationInfoModel.getAuthorizationStatus() == null) {
@@ -45,34 +46,86 @@ public class AuthorizationInfoService {
         if (authorizationId != null) {
             return authorizationInfoModel;
         } else {
-            throw new Exception("权限添加失败");
+            throw new BizException("权限添加失败");
+        }
+    }
+
+    /**
+     * 添加 权限 信息
+     *
+     * @param authorizationInfoModel
+     * @return
+     * @throws Exception
+     */
+    public AuthorizationInfoModel editAuthorizationInfo(AuthorizationInfoModel authorizationInfoModel) throws Exception {
+        if (isAuthorizationName(AuthorizationConstant.BasicOperation.EDIT, authorizationInfoModel.getAuthorizationName())) {
+            throw new BizException("权限名称重复");
+        }
+        if (isAuthorizationName(AuthorizationConstant.BasicOperation.EDIT, authorizationInfoModel.getAuthorizationCode())) {
+            throw new BizException("权限编码重复");
+        }
+        if (authorizationInfoModel.getAuthorizationStatus() == null) {
+            authorizationInfoModel.setAuthorizationStatus(AuthorizationInfoModel.AuthorizationStatus.START.getAuthorizationStatus());
+        }
+        Integer authorizationId = authorizationInfoModelMapper.addAuthorizationInfo(authorizationInfoModel);
+        if (authorizationId != null) {
+            return authorizationInfoModel;
+        } else {
+            throw new BizException("权限添加失败");
         }
     }
 
     /**
      * 权限名称是否重复(true重复,false不重复)
+     *
      * @param authorizationName
      * @return
      */
-    public boolean isAuthorizationName(AuthorizationConstant.BasicOperation basicOperation, Integer authorizationId,String authorizationName) {
-        if(AuthorizationConstant.BasicOperation.ADD == basicOperation){
-            return authorizationInfoModelMapper.getListByAuthorizationName(authorizationName).size() > 0 ;
+    public boolean isAuthorizationName(AuthorizationConstant.BasicOperation basicOperation, String authorizationName) {
+        List<AuthorizationInfoModel> list = authorizationInfoModelMapper.getListByAuthorizationName(authorizationName);
+        if (CollectionUtils.isEmpty(list)) {
+            return false;
         }
-        if(AuthorizationConstant.BasicOperation.EDIT == basicOperation){
-            List<AuthorizationInfoModel> list = authorizationInfoModelMapper.getListByAuthorizationName(authorizationName);
-            if(!CollectionUtils.isEmpty(list)){
 
+        if (AuthorizationConstant.BasicOperation.ADD == basicOperation) {
+            return list.size() > 0;
+        }
+
+        if (AuthorizationConstant.BasicOperation.EDIT == basicOperation) {
+            for (int i = 0; i < list.size(); i++) {
+                AuthorizationInfoModel tempAuthorizationInfoModel = list.get(i);
+                if (tempAuthorizationInfoModel.getAuthorizationName() != authorizationName) {
+                    return true;
+                }
             }
         }
-        return true;
+        return false;
     }
 
     /**
      * 权限编码是否重复(true重复,false不重复)
+     *
      * @param authorizationCode
      * @return
      */
-    public boolean isAuthorizationCode(String authorizationCode) {
-        return true;
+    public boolean isAuthorizationCode(AuthorizationConstant.BasicOperation basicOperation, String authorizationCode) {
+        List<AuthorizationInfoModel> list = authorizationInfoModelMapper.getListByAuthorizationCode(authorizationCode);
+        if (CollectionUtils.isEmpty(list)) {
+            return false;
+        }
+
+        if (AuthorizationConstant.BasicOperation.ADD == basicOperation) {
+            return list.size() > 0;
+        }
+
+        if (AuthorizationConstant.BasicOperation.EDIT == basicOperation) {
+            for (int i = 0; i < list.size(); i++) {
+                AuthorizationInfoModel tempAuthorizationInfoModel = list.get(i);
+                if (tempAuthorizationInfoModel.getAuthorizationCode() != authorizationCode) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
