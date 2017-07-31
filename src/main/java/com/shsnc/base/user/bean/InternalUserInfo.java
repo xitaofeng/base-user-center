68b59ef9c6aebd39166abfa331357dea247d024f
侯光强 <houguangqiang@shsnc.com>
2017-07-31 16:22:43 +0800

    【更新】更新验证token返回的对象信息

---
+++ src/main/java/com/shsnc/base/user/bean/InternalUserInfo.java
@@ -0,0 +1,84 @@
+package com.shsnc.base.user.bean;
+
+import java.util.List;
+
+/**
+ * @author houguangqiang
+ * @date 2017-07-31
+ * @since 1.0
+ */
+public class InternalUserInfo {
+
+    private Long userId;
+    private String username;
+    private String alias;
+    private String mobile;
+    private String email;
+    private Integer internal;
+    private Integer status;
+    private List<Long> groupIds;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getInternal() {
+        return internal;
+    }
+
+    public void setInternal(Integer internal) {
+        this.internal = internal;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public List<Long> getGroupIds() {
+        return groupIds;
+    }
+
+    public void setGroupIds(List<Long> groupIds) {
+        this.groupIds = groupIds;
+    }
+}
