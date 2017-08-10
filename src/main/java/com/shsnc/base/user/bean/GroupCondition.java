fcefd226c340afb497f9c5f2399957be35c3dde5
侯光强 <houguangqiang@shsnc.com>
2017-08-10 17:44:53 +0800

    【更新】用户组移除数据权限控制

--- src/main/java/com/shsnc/base/user/bean/GroupCondition.java
+++ src/main/java/com/shsnc/base/user/bean/GroupCondition.java
@@ -1,13 +1,9 @@
 package com.shsnc.base.user.bean;
 
-import java.util.List;
-
 public class GroupCondition {
 
     private String name;
     private String code;
-    private boolean checkPermission;
-    private List<Long> objectIds;
 
     public String getName() {
         return name;
@@ -25,19 +21,4 @@ public class GroupCondition {
         this.code = code;
     }
 
-    public boolean isCheckPermission() {
-        return checkPermission;
-    }
-
-    public void setCheckPermission(boolean checkPermission) {
-        this.checkPermission = checkPermission;
-    }
-
-    public List<Long> getObjectIds() {
-        return objectIds;
-    }
-
-    public void setObjectIds(List<Long> objectIds) {
-        this.objectIds = objectIds;
-    }
 }
