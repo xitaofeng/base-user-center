1555e1e5d94fb0390dd651d4a609b2dd34ba092d
侯光强 <houguangqiang@shsnc.com>
2017-08-10 18:06:10 +0800

    【新增】请求授权服务工具类

---
+++ src/main/java/com/shsnc/base/authorization/handler/AuthorizationResourceGroupHandler.java
@@ -0,0 +1,75 @@
+package com.shsnc.base.authorization.handler;
+
+import com.shsnc.api.core.RequestHandler;
+import com.shsnc.api.core.annotation.Authentication;
+import com.shsnc.api.core.annotation.LoginRequired;
+import com.shsnc.api.core.annotation.RequestMapper;
+import com.shsnc.api.core.validation.Validate;
+import com.shsnc.base.authorization.config.DataObject;
+import com.shsnc.base.authorization.config.DataOperation;
+import com.shsnc.base.authorization.service.AuthorizationRightsService;
+import com.shsnc.base.util.config.BaseException;
+import com.shsnc.base.util.config.BizException;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * @author houguangqiang
+ * @date 2017-08-09
+ * @since 1.0
+ */
+@Component
+@LoginRequired
+@RequestMapper("/authorization/resourceGroup")
+public class AuthorizationResourceGroupHandler implements RequestHandler {
+
+    @Autowired
+    private AuthorizationRightsService authorizationRightsService;
+
+    /**
+     * 授权编排
+     */
+    @RequestMapper("/addRights")
+    @Validate
+    @Authentication("BASE_USER_CENTER_AUTHORIZATION_RESOURCE_GROUP_ADD_RIGHTS")
+    public boolean addRights(@NotNull Long resourceGroupId, @NotNull List<Long> groupIds) throws BaseException {
+        authorizationRightsService.authorize(DataObject.RESOURCE_GROUP, resourceGroupId, groupIds, DataOperation.ALL, false);
+        return true;
+    }
+    /**
+     * 授权编排
+     */
+    @RequestMapper("/updateRights")
+    @Validate
+    @Authentication("BASE_USER_CENTER_AUTHORIZATION_RESOURCE_GROUP_UPDATE_RIGHTS")
+    public boolean updateRights(@NotNull Long resourceGroupId, @NotNull List<Long> groupIds) throws BaseException {
+        authorizationRightsService.authorize(DataObject.RESOURCE_GROUP, resourceGroupId, groupIds, DataOperation.ALL, true);
+        return true;
+    }
+
+    @RequestMapper("/checkOne")
+    @Validate
+    @Authentication("BASE_USER_CENTER_AUTHORIZATION_RESOURCE_GROUP_CHECK_ONE")
+    public boolean checkOne(@NotNull Long resourceGroupId){
+        return authorizationRightsService.checkPermisson(DataObject.RESOURCE_GROUP, resourceGroupId, DataOperation.ALL);
+    }
+
+    @RequestMapper("/checkMany")
+    @Validate
+    @Authentication("BASE_USER_CENTER_AUTHORIZATION_RESOURCE_GROUP_CHECK_Many")
+    public boolean checkMany(@NotEmpty List<Long> resourceGroupIds){
+        return authorizationRightsService.checkPermisson(DataObject.RESOURCE_GROUP, resourceGroupIds, DataOperation.ALL);
+    }
+
+    @RequestMapper("/getAllIds")
+    @Validate
+    @Authentication("BASE_USER_CENTER_AUTHORIZATION_RESOURCE_GROUP_GET_ALL_IDS")
+    public List<Long> getAllIds() throws BizException {
+        return authorizationRightsService.getObjectIds(DataObject.RESOURCE_GROUP, DataOperation.ALL);
+    }
+
+}
