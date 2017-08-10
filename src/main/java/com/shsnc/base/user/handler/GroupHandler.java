808861268b3195f2778f0bd6d8ab50311534e3f8
侯光强 <houguangqiang@shsnc.com>
2017-08-10 15:20:19 +0800

    【更新】用户组添加权限过滤

--- src/main/java/com/shsnc/base/user/handler/GroupHandler.java
+++ src/main/java/com/shsnc/base/user/handler/GroupHandler.java
@@ -11,9 +11,10 @@ import com.shsnc.base.user.bean.Group;
 import com.shsnc.base.user.bean.GroupCondition;
 import com.shsnc.base.user.model.GroupModel;
 import com.shsnc.base.user.service.GroupService;
-import com.shsnc.base.util.BizAssert;
 import com.shsnc.base.util.JsonUtil;
+import com.shsnc.base.util.config.BaseException;
 import com.shsnc.base.util.config.BizException;
+import com.shsnc.base.util.config.MessageCode;
 import com.shsnc.base.util.sql.Pagination;
 import com.shsnc.base.util.sql.QueryData;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -22,6 +23,7 @@ import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -61,16 +63,17 @@ public class GroupHandler implements RequestHandler {
     @RequestMapper("/getObject")
     @Validate
     @Authentication("BASE_USER_GROUP_GET_OBJECT")
-    public Group getObject(@NotNull Long groupId) throws BizException {
+    public Group getObject(@NotNull Long groupId) throws BaseException {
         if (!ThreadContext.getUserInfo().isSuperAdmin()) {
             List<Long> groupIds = ThreadContext.getUserInfo().getGroupIds();
-            if (!groupIds.isEmpty()) {
-                BizAssert.isTrue(groupIds.contains(groupId), "没有权限");
-            } else {
-                return null;
+            if (!groupIds.contains(groupId)) {
+                throw new BaseException(MessageCode.PERMISSION_DENIED);
             }
         }
         GroupModel groupModel = groupService.getGroup(groupId);
+        List<GroupModel> groupModels = Collections.singletonList(groupModel);
+        groupService.selectUsers(groupModels);
+        groupService.selectResourceGroups(groupModels);
         return JsonUtil.convert(groupModel, Group.class);
     }
 
@@ -103,10 +106,12 @@ public class GroupHandler implements RequestHandler {
     @RequestMapper("/update")
     @Validate(groups = ValidationType.Update.class)
     @Authentication("BASE_USER_GROUP_UPDATE")
-    public boolean update(Group group) throws BizException {
+    public boolean update(Group group) throws BaseException {
         if (!ThreadContext.getUserInfo().isSuperAdmin()) {
             List<Long> groupIds = ThreadContext.getUserInfo().getGroupIds();
-            BizAssert.isTrue(groupIds.contains(group.getGroupId()), "权限不足");
+            if (!groupIds.contains(group.getGroupId())) {
+                throw new BaseException(MessageCode.PERMISSION_DENIED);
+            }
         }
         GroupModel groupModel = JsonUtil.convert(group,GroupModel.class);
         return groupService.updateGroup(groupModel);
@@ -115,10 +120,12 @@ public class GroupHandler implements RequestHandler {
     @RequestMapper("/delete")
     @Validate
     @Authentication("BASE_USER_GROUP_DELETE")
-    public boolean delete(@NotNull Long groupId) throws BizException {
+    public boolean delete(@NotNull Long groupId) throws BaseException {
         if (!ThreadContext.getUserInfo().isSuperAdmin()) {
             List<Long> groupIds = ThreadContext.getUserInfo().getGroupIds();
-            BizAssert.isTrue(groupIds.contains(groupId), "权限不足");
+            if (!groupIds.contains(groupId)) {
+                throw new BaseException(MessageCode.PERMISSION_DENIED);
+            }
         }
         return groupService.deleteGroup(groupId);
     }
@@ -126,10 +133,12 @@ public class GroupHandler implements RequestHandler {
     @RequestMapper("/batchDelete")
     @Validate
     @Authentication("BASE_USER_GROUP_BATCH_DELETE")
-    public boolean batchDelete(@NotEmpty List<Long> groupIds) throws BizException {
+    public boolean batchDelete(@NotEmpty List<Long> groupIds) throws BaseException {
         if (!ThreadContext.getUserInfo().isSuperAdmin()) {
             List<Long> currentGroupIds = ThreadContext.getUserInfo().getGroupIds();
-            BizAssert.isTrue(currentGroupIds.containsAll(groupIds), "权限不足");
+            if (!currentGroupIds.containsAll(groupIds)) {
+                throw new BaseException(MessageCode.PERMISSION_DENIED);
+            }
         }
         return groupService.batchDeleteGroup(groupIds);
     }
@@ -137,10 +146,12 @@ public class GroupHandler implements RequestHandler {
     @RequestMapper("/assignUsers")
     @Validate
     @Authentication("BASE_USER_GROUP_ASSIGN_USERS")
-    public boolean assignUsers(@NotNull Long groupId, @NotEmpty List<Long> userIds) throws BizException {
+    public boolean assignUsers(@NotNull Long groupId, @NotEmpty List<Long> userIds) throws BaseException {
         if (!ThreadContext.getUserInfo().isSuperAdmin()) {
             List<Long> groupIds = ThreadContext.getUserInfo().getGroupIds();
-            BizAssert.isTrue(groupIds.contains(groupId), "权限不足");
+            if (!groupIds.contains(groupId)) {
+                throw new BaseException(MessageCode.PERMISSION_DENIED);
+            }
         }
         return groupService.assignUsers(groupId, userIds);
     }
@@ -148,10 +159,12 @@ public class GroupHandler implements RequestHandler {
     @RequestMapper("/assignReresourceGroups")
     @Validate
     @Authentication("BASE_USER_GROUP_ASSIGN_RERESOURCE_GROUPS")
-    public boolean assignReresourceGroups(@NotNull Long groupId, @NotEmpty List<Long> reresourceGroupIds) throws BizException {
+    public boolean assignReresourceGroups(@NotNull Long groupId, @NotEmpty List<Long> reresourceGroupIds) throws BaseException {
         if (!ThreadContext.getUserInfo().isSuperAdmin()) {
             List<Long> groupIds = ThreadContext.getUserInfo().getGroupIds();
-            BizAssert.isTrue(groupIds.contains(groupId), "权限不足");
+            if (!groupIds.contains(groupId)) {
+                throw new BaseException(MessageCode.PERMISSION_DENIED);
+            }
         }
         return groupService.assignReresourceGroups(groupId, reresourceGroupIds);
     }
