c25d30a228e21712780a5f1d2d408c269bd8449f
zhouchunyi <zhouchunyi@shsnc.com>
2017-09-05 15:26:47 +0800

    更新登录日志呈现

--- src/main/java/com/shsnc/base/user/handler/LoginHistoryHandler.java
+++ src/main/java/com/shsnc/base/user/handler/LoginHistoryHandler.java
@@ -3,6 +3,13 @@ package com.shsnc.base.user.handler;
 import com.shsnc.api.core.RequestHandler;
 import com.shsnc.api.core.annotation.LoginRequired;
 import com.shsnc.api.core.annotation.RequestMapper;
+import com.shsnc.base.user.bean.UserInfo;
+import com.shsnc.base.user.model.LoginHistoryModel;
+import com.shsnc.base.user.service.LoginHistoryService;
+import com.shsnc.base.util.sql.Pagination;
+import com.shsnc.base.util.sql.QueryData;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,4 +19,16 @@ import org.springframework.stereotype.Component;
 @RequestMapper("/user/loginHistory")
 @LoginRequired
 public class LoginHistoryHandler implements RequestHandler {
+    
+    @Autowired
+    private LoginHistoryService loginHistoryService;
+    
+    @RequestMapper("/getPage")
+    public QueryData getPage(LoginHistoryModel condition, Pagination pagination){
+       
+        QueryData queryData = loginHistoryService.getLoginHistoryPage(condition, pagination);
+        
+        return queryData;
+    }
+
 }
