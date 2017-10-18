1de2593bd7161454813c687c3228adca641974e6
侯光强 <houguangqiang@shsnc.com>
2017-10-18 12:23:57 +0800

    【优化】添加权限handler

--- src/main/java/com/shsnc/base/authorization/config/DataObject.java
+++ src/main/java/com/shsnc/base/authorization/config/DataObject.java
@@ -1,5 +1,7 @@
 package com.shsnc.base.authorization.config;
 
+import com.shsnc.base.util.config.BizException;
+
 /**
  * @author houguangqiang
  * @date 2017-08-08
@@ -7,17 +9,25 @@ package com.shsnc.base.authorization.config;
  */
 public enum DataObject {
 
+    /** 资源组 */
     RESOURCE_GROUP(1, "资源组"),
+    /** 脚本 */
     SCRIPT(2, "脚本"),
+    /** 编排 */
     ORCHESTRATION(3, "编排"),
+    /** 巡检 */
     INSPECTION(4, "巡检"),
+    /** 巡检项 */
+    INSPECTION_ITEM(5, "巡检项"),
+    /** 作业 */
+    TASK(6, "作业"),
 
     ;
 
     private int type;
     private String description;
 
-    private DataObject(int type, String description){
+    DataObject(int type, String description){
         this.type = type;
         this.description = description;
     }
@@ -28,5 +38,13 @@ public enum DataObject {
 
     public String getDescription() {
         return description;
+    }
+
+    public static DataObject of(String name) throws BizException {
+        try {
+            return DataObject.valueOf(name);
+        } catch (IllegalArgumentException e) {
+            throw new BizException("不支持的权限数据来源！");
+        }
     }
 }
